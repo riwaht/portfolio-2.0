ff72c7ee8c5097271d873affeeeb772d368abf7c
Riwa Hoteit <riwa.hoteit@gmail.com>
2024-05-22 19:27:12 +0300

    Setup Files

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+
+function Home() {
+    return (
+        <Canvas>
+            
+        </Canvas>
+    )
+}
+
+export default Home
