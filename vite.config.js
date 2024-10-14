f384061b8866335923d948f8abc021644d9529bf
riwaht <riwa.hoteit@lau.edu>
2024-10-14 23:52:25 +0300

    Bind to correct host in config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-})
+  base: "/",
+  server: {
+    host: "0.0.0.0", // Allow access from any IP
+    port: 5173, // Ensure this matches the port you're trying to access
+    strictPort: true, // Ensure it fails if the port is already in use
+  },
+});
